3a5851d5a92577126c8c8f99890df44dd03e5d45
Jorrit Stutterheim <jorrit@Jorrits-MBP.home>
2022-12-04 11:06:37 +0100

    C# setup

---
+++ 2022/AoCDotNet/AoCDotNet/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AoCDotNet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filename = @"test.txt";
+        var lines = File.ReadLines(filename);
+
+        foreach (var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+        Console.Write("Press ENTER to exit");
+        Console.ReadLine();
+    }
+}
