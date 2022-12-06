fc2725f20d4452f5c059e786843062f5e6cf29f3
Jorrit Stutterheim <jorrit@sp-byods-145-109-40-205.wireless.uva.nl>
2022-12-06 11:21:22 +0100

    day 6

---
+++ 2022/Day6/Day6.hs
@@ -0,0 +1,16 @@
+module Day6 where
+
+import System.IO
+import Control.Monad
+import Data.List
+
+duplicates :: String -> Bool
+duplicates str = length (nub str) == length str
+
+parseString :: String -> Int -> Int -> Int
+parseString str counter method | duplicates $ take method str = counter + method
+                               | otherwise = parseString (drop 1 str) (counter + 1) method
+main = do
+    fileContent <- readFile "input.txt"
+    let dataStream =  head $ words fileContent
+    print (parseString dataStream 0 4, parseString dataStream 0 14)
