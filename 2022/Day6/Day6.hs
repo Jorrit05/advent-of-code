bd8b8b642bded5b1d91cfcb88eb817c0cda42efd
Jorrit Stutterheim <jorrit@uvavpn-byods-145-109-51-30.vpn.uva.nl>
2022-12-06 11:46:05 +0100

    day 6

--- 2022/Day6/Day6.hs
+++ 2022/Day6/Day6.hs
@@ -6,8 +6,8 @@ duplicates :: String -> Bool
 duplicates str = length (nub str) == length str
 
 parseString :: String -> Int -> Int -> Int
-parseString str counter method | duplicates $ take method str = counter + method
-                               | otherwise = parseString (drop 1 str) (counter + 1) method
+parseString str counter marker | duplicates $ take marker str = counter + marker
+                               | otherwise = parseString (drop 1 str) (counter + 1) marker
 main = do
     fileContent <- readFile "input.txt"
     let dataStream =  head $ words fileContent
