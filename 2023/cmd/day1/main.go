55535c24ca58c422804455b82a7e612ff9d5a08d
Jorrit05 <jorstut@hotmail.com>
2023-12-04 23:05:31 +0100

    Solution puzzle 2

--- 2023/cmd/day1/main.go
+++ 2023/cmd/day1/main.go
@@ -2,91 +2,86 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
-	"strings"
+	"time"
 
 	"github.com/Jorrit05/advent-of-code/pkg/lib"
 )
 
+var digitMap = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
 func main() {
-	input, err := lib.GetInput("sample.txt", false)
+	startTime := time.Now()
+	input, err := lib.GetInput("input.txt", false)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	totals := 0
+	totalsPuzzle1 := 0
+	totalsPuzzle2 := 0
 
-	var iterator [][]string
-	puzzleOne := false
-	if puzzleOne {
-		iterator = input.Lines
-	} else {
-		iterator = replaceNumberStrings(*input)
-
+	for _, word := range input.Lines {
+		puzzle1Numbers := getNumbersOnline(word)
+		totalsPuzzle1 += extractValue(puzzle1Numbers)
 	}
-	// Puzzle 1:
-	for _, word := range iterator {
-		var numbersOnLine []int
-
-		numbersOnLine = getNumbersOnline(word, numbersOnLine)
-		totals += extractValue(numbersOnLine)
-	}
-	fmt.Println(totals)
-}
-
-// Puzzle 2:
-func replaceNumberStrings(input lib.PuzzleInput) [][]string {
-	digitMap := map[string]string{
-		"one":   "1",
-		"two":   "2",
-		"three": "3",
-		"four":  "4",
-		"five":  "5",
-		"six":   "6",
-		"seven": "7",
-		"eight": "8",
-		"nine":  "9",
-	}
-	var newlines [][]string
 
 	for _, word := range input.StringLines {
-
-		output := replaceFirstLastOccurrences(word, digitMap)
-		fmt.Println(output)
-		// Iterate over each word in input.StringLines
-		// Append the final result to newlines
-		lineSlice := strings.Split(output, "")
-		newlines = append(newlines, lineSlice)
+		puzzle2Numbers := getPuzzleTwoNumbers(word)
+		totalsPuzzle2 += extractValue(puzzle2Numbers)
 	}
-	// Compile regular expressions for each word in the map
-	// regexMap := make(map[*regexp.Regexp]string)
-	// for word, digit := range digitMap {
-	// 	// Regular expression pattern matching the word directly
-	// 	pattern := word
-	// 	r := regexp.MustCompile(pattern)
-	// 	regexMap[r] = digit
-	// }
 
-	// var newlines [][]string
+	fmt.Printf("Puzzle 1: %d\n", totalsPuzzle1)
+	fmt.Printf("Puzzle 2: %d\n", totalsPuzzle2)
 
-	// // Iterate over each word in input.StringLines
-	// for _, word := range input.StringLines {
-	// 	line := word
-
-	// 	// Apply all regex replacements to the line
-	// 	for regex, digit := range regexMap {
-	// 		line = regex.ReplaceAllString(line, digit)
-	// 	}
-
-	// 	// Append the final result to newlines
-	// 	lineSlice := strings.Split(line, "")
-	// 	newlines = append(newlines, lineSlice)
-	// }
-	// fmt.Println(newlines)
-	return newlines
+	duration := time.Since(startTime)
+	fmt.Println("Execution time: ", duration)
 }
 
-func getNumbersOnline(word []string, numbersOnLine []int) []int {
+func getPuzzleTwoNumbers(word string) []int {
+	pattern := "\\d|one|two|three|four|five|six|seven|eight|nine"
+	reversePattern := "\\d|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno"
+	r := regexp.MustCompile(pattern)
+	reverserR := regexp.MustCompile(reversePattern)
+
+	first := string(r.FindAll([]byte(word), -1)[0])
+	last := reverseString(string(reverserR.FindAll([]byte(reverseString(word)), -1)[0]))
+
+	first = getDigitValue(first)
+	last = getDigitValue(last)
+
+	fst, _ := strconv.Atoi(first)
+	lst, _ := strconv.Atoi(last)
+	return []int{fst, lst}
+}
+
+func getDigitValue(key string) string {
+	if len(key) > 1 {
+		return digitMap[key]
+	}
+	return key
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func getNumbersOnline(word []string) []int {
+	var numbersOnLine []int
 	for _, letter := range word {
 		if val, err := strconv.Atoi(letter); err == nil {
 			numbersOnLine = append(numbersOnLine, val)
@@ -105,28 +100,4 @@ func extractValue(numbersOnLine []int) int {
 
 func calcValue(a int, b int) int {
 	return (a * 10) + b
-}
-
-func replaceFirstLastOccurrences(s string, digitMap map[string]string) string {
-	for word, digit := range digitMap {
-		s = replaceFirstOccurrence(s, word, digit)
-		s = replaceLastOccurrence(s, word, digit)
-	}
-	return s
-}
-
-func replaceFirstOccurrence(s, word, replacement string) string {
-	idx := strings.Index(s, word)
-	if idx != -1 {
-		return s[:idx] + replacement + s[idx+len(word):]
-	}
-	return s
-}
-
-func replaceLastOccurrence(s, word, replacement string) string {
-	idx := strings.LastIndex(s, word)
-	if idx != -1 {
-		return s[:idx] + replacement + s[idx+len(word):]
-	}
-	return s
 }
