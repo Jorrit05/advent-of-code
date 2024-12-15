4ce4e437df75ac44f7ebef10b42164291a2cff39
Jorrit Stutterheim <jorrit.stutterheim@tno.nl>
2024-12-15 11:30:08 +0100

    day 5 part 12

--- advent2024/lib/days/day05.ex
+++ advent2024/lib/days/day05.ex
@@ -3,13 +3,73 @@ defmodule Advent2024.Days.Day05 do
   Solution for Day 05 of Advent of Code 2024.
   """
 
+  require Logger
   alias Advent2024.Utils
 
   def get_input(type \\ :sample) do
-    Utils.get_raw_file(Utils.get_input(type, __MODULE__))
-    |> String.split("\n")
+    [rules | updates] =
+      Utils.get_raw_file(Utils.get_input(type, __MODULE__))
+      |> String.split("\n\n")
+
+    rule_map =
+      rules
+      |> String.split(["\n"])
+      |> Enum.map(fn line ->
+        [left, right] = String.split(line, "|")
+        {String.to_integer(left), String.to_integer(right)}
+      end)
+
+    update_instructions =
+      updates
+      |> List.first()
+      |> String.split(["\n"])
+      |> Enum.map(fn line ->
+        line
+        |> String.split(",")
+        |> Enum.map(&String.to_integer/1)
+      end)
+
+    {rule_map, update_instructions}
   end
 
-  def part1(data) do
+  def process_line(_rule_set, [_]) do
+    true
+  end
+
+  def process_line(rule_set, [fst, snd | tail]) do
+    cond do
+      MapSet.member?(rule_set, {snd, fst}) -> false
+      true -> process_line(rule_set, [snd | tail])
+    end
+  end
+
+  defp get_middle_number(line) do
+    Enum.at(line, div(length(line), 2))
+  end
+
+  def part1(rule_set, update_instructions) do
+    update_instructions
+    |> Enum.reduce({0, []}, fn line, {counter, incorrect_updates} ->
+      cond do
+        process_line(rule_set, line) ->
+          {counter + get_middle_number(line), incorrect_updates}
+
+        true ->
+          {counter, [line] ++ incorrect_updates}
+      end
+    end)
+  end
+
+  def part2(rule_set, update_instructions) do
+    update_instructions
+    |> Enum.reduce({0, []}, fn line, {counter, incorrect_updates} ->
+      cond do
+        process_line(rule_set, line) ->
+          {counter + get_middle_number(line), incorrect_updates}
+
+        true ->
+          {counter, [line] ++ incorrect_updates}
+      end
+    end)
   end
 end
--- advent2024/scripts/day05.exs
+++ advent2024/scripts/day05.exs
@@ -9,6 +9,13 @@ input_type =
     _ -> :sample
   end
 
-data = Day05.get_input(input_type)
+{rule_map, update_instructions} = Day05.get_input(input_type)
 
-Day05.part1(data) |> IO.inspect(label: "Part 1: ")
+rule_set = MapSet.new(rule_map)
+
+{results, incorrect_updates} =
+  Day05.part1(rule_set, update_instructions)
+
+results |> IO.inspect(label: "Part 1: ")
+
+Day05.part2(rule_set, incorrect_updates)
